3b441d023f38fb81d46195e1f26e164ab2fcfe8d
Giorgi <meskhi10@gmail.com>
2021-07-06 23:20:32 +0200

    -hand merge camera view controller

---
+++ SecondTeamWork/Presentation/Scenes/Camera/ViewModel/CameraDataSource.swift
@@ -0,0 +1,55 @@
+//
+//  CameraDataSource.swift
+//  SecondTeamWork
+//
+//  Created by Ilia Tsikelashvili on 07.07.21.
+//
+
+import Foundation
+import UIKit
+
+class CameraDataSource : NSObject {
+    
+    private var viewModel: CameraViewModelProtocol!
+    private var coordinator: CoordinatorProtocol!
+//    private var albums = [String]()
+    private var images = [UIImage]()
+    private var imageManager : ImagesFileManager!
+    private var imageView = UIImage()
+    
+    
+    init(with viewModel: CameraViewModelProtocol, coordinator: CoordinatorProtocol, imageView : UIImage) {
+        super.init()
+        self.viewModel   = viewModel
+        self.coordinator = coordinator
+        self.imageView   = imageView
+    }
+ 
+    func refresh() {
+        do {
+            images = try viewModel.fetchGalleryImages()
+        } catch {
+            print("Error")
+        }
+    }
+}
+
+extension CameraDataSource : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let id = UUID.init().uuidString
+        guard let img = imageManager else {return}
+        do {
+            try img.saveImageInGallery(imgName: "\(id)", image: imageView)
+        }
+        catch {
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        imageView = image
+    }
+}
---
+++ SecondTeamWork/Presentation/Scenes/Camera/ViewModel/CameraViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  CameraViewModel.swift
+//  SecondTeamWork
+//
+//  Created by Ilia Tsikelashvili on 07.07.21.
+//
+
+import Foundation
+import UIKit
+
+protocol CameraViewModelProtocol: AnyObject {
+    func fetchGalleryImages() throws -> [UIImage]
+    var controller: CoordinatorDelegate { get }
+    init(controller: CoordinatorDelegate)
+}
+
+final class CameraViewModel : CameraViewModelProtocol {
+    
+    private(set) var controller: CoordinatorDelegate
+    
+    init(controller: CoordinatorDelegate) {
+        self.controller = controller
+    }
+    
+    func fetchGalleryImages() throws -> [UIImage] {
+        var images = GalleryModel().images
+       
+        return images
+    }
+
+}
