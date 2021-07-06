0b878c3e6ba3b13dc176bcfd2579b95116c989a1
tsikk <tsikelashviliilia@gmail.com>
2021-07-07 01:08:36 +0400

    -changed into mvvm pattern

---
+++ SecondTeamWork/Presentation/Scenes/Camera/View/CameraViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CameraViewController.swift
+//  SecondTeamWork
+//
+//  Created by Ilia Tsikelashvili on 06.07.21.
+//
+
+import UIKit
+
+class CameraViewController: BaseViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imgView: UIImageView!
+    private var cameraViewModel: CameraViewModelProtocol!
+    private var cameraDataSource: CameraDataSource!
+    
+    private var imageManager : ImagesFileManager!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imgView.backgroundColor = .clear
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureViewModel()
+
+    }
+   
+
+    @IBAction func didTapButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker,animated: true)
+    }
+    
+    private func configureViewModel() {
+        cameraViewModel = CameraViewModel(controller: self)
+        cameraDataSource = CameraDataSource(with: cameraViewModel, coordinator: coordinator!, imageView: imgView.image!)
+        cameraDataSource.refresh()
+    }
+}
+
+
+
+
